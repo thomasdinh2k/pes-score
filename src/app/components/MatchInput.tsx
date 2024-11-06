49a41e18ce05125929fd2ba6713d5f5b1b6447a2
thomasdinh <work.dnstung@gmail.com>
2024-11-06 08:57:40 +0700

    add input

--- src/app/components/MatchInput.tsx
+++ src/app/components/MatchInput.tsx
@@ -1,13 +1,30 @@
+"use client";
+import { Form, Input, Select, Space } from "antd";
 import React from "react";
 
 type Props = {};
 
 function MatchInput({}: Props) {
-	return (
+    
+    return (
 		<div className="match-input">
 			<h2>Match Input</h2>
 
-            
+			<Form layout="vertical">
+				{/* Player name */}
+				<span className=" font-semibold">Player Home</span>
+				<Input name="player-1-name" placeholder="Thomas"></Input>
+
+				<Space.Compact>
+					<Input
+						name="player-1-teamName"
+						placeholder="Liverpool..."
+					></Input>
+					<Input type="number" defaultValue={0}></Input>
+                    <button className="px-4 border">+</button>
+                    <button className="px-4 border">-</button>
+				</Space.Compact>
+			</Form>
 		</div>
 	);
 }
