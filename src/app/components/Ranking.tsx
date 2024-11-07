46fa004213b7e3a4ddcde108299850c5b217bc01
Thomas Dinh <work.dnstung@gmail.com>
2024-11-07 07:11:29 +0700

    rebase from origin/dev (#1)
    This is only serve for feature "Display Ranking"

--- src/app/components/Ranking.tsx
+++ src/app/components/Ranking.tsx
@@ -1,11 +1,108 @@
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
+import type { FetchedData, Ranking } from "../types/data.type";
+import { Table } from "antd";
+import type { TableProps } from "antd";
 
-type Props = {}
+const Ranking = () => {
+	const [rankingData, setRankingData] = useState<FetchedData>();
+	const [loading, setLoading] = useState<boolean>(true);
 
-const Ranking = (props: Props) => {
-  return (
-    <div>Ranking</div>
-  )
-}
+	const fetchData = async () => {
+		const response = await fetch("/data/ranking.json");
+		const data = await response.json();
+		setRankingData(data);
+		setLoading(false);
+	};
 
-export default Ranking
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const columns: TableProps<Ranking>["columns"] = [
+		{
+			title: "Rank",
+			dataIndex: "rank",
+			key: "rank",
+			render: (_, __, index) => <span>{index + 1}</span>,
+		},
+		{
+			title: "Player",
+			dataIndex: "player",
+			key: "player",
+		},
+		{
+			title: "Wins",
+			dataIndex: "wins",
+			key: "wins",
+		},
+		{
+			title: "Draws",
+			dataIndex: "draws",
+			key: "draws",
+		},
+		{
+			title: "Losses",
+			dataIndex: "losses",
+			key: "losses",
+		},
+		{
+			title: "Goal for/against",
+			dataIndex: "goal_difference",
+			key: "goal_difference",
+			render: (_, record) => (
+				<span>
+					{record.goals_for}/{record.goals_against}
+				</span>
+			),
+		},
+		{
+			title: "Goal Difference",
+			dataIndex: "goal_difference",
+			key: "goal_difference",
+		},
+		{
+			title: "Points",
+			dataIndex: "points",
+			key: "points",
+		},
+	];
+
+	// const data: FetchedData['ranking'] = [
+	// 	{
+	// 		key: "1",
+	// 		name: "John Brown",
+	// 		age: 32,
+	// 		address: "New York No. 1 Lake Park",
+	// 		tags: ["nice", "developer"],
+	// 	},
+	// 	{
+	// 		key: "2",
+	// 		name: "Jim Green",
+	// 		age: 42,
+	// 		address: "London No. 1 Lake Park",
+	// 		tags: ["loser"],
+	// 	},
+	// 	{
+	// 		key: "3",
+	// 		name: "Joe Black",
+	// 		age: 32,
+	// 		address: "Sydney No. 1 Lake Park",
+	// 		tags: ["cool", "teacher"],
+	// 	},
+	// ];
+
+	return (
+		<>
+			<h2>Ranking</h2>
+
+			<Table<Ranking>
+				columns={columns}
+				dataSource={rankingData?.ranking}
+				loading={loading}
+			/>
+		</>
+	);
+};
+
+export default Ranking;
---
+++ src/app/types/data.type.ts
@@ -0,0 +1,26 @@
+export interface FetchedData {
+	ranking: Ranking[];
+	matches: Match[];
+}
+
+export interface Ranking {
+	rank: number;
+	player: string;
+	wins: number;
+	draws: number;
+	losses: number;
+	goals_for: number;
+	goals_against: number;
+	goal_difference: number;
+	points: number;
+}
+
+export interface Match {
+	match_number: number;
+	home_player: string;
+	home_team?: string;
+	home_score: number;
+	away_player: string;
+	away_team?: string;
+	away_score: number;
+}
