9fbf8bee81791bc16d52827d7dc7148531758379
thomasdinh <work.dnstung@gmail.com>
2024-11-10 20:44:00 +0700

    implement switch (toggle)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,15 @@ import MatchHistory from "./components/MatchHistory";
 import MatchInput from "./components/MatchInput";
 import Ranking from "./components/Ranking";
 import type { FetchedData } from "./types/data.type";
-import { Skeleton } from "antd";
+import { Skeleton, Space, Switch } from "antd";
 
 export default function Home() {
 	const [data, setData] = useState<FetchedData>();
 	const [loading, setLoading] = useState<boolean>(true);
+	const [isShow, setIsShow] = useState<{
+		ranking: boolean;
+		matches: boolean;
+	}>({ ranking: true, matches: true });
 
 	const fetchData = async () => {
 		try {
@@ -40,12 +44,39 @@ export default function Home() {
 				<MatchInput />
 			</section>
 
+			<Space direction="horizontal">
+				<Switch
+					checkedChildren="Show Ranking"
+					unCheckedChildren="Hide Ranking"
+					defaultChecked
+					onChange={(checked) =>
+						setIsShow((prevState) => ({
+							...prevState,
+							ranking: checked,
+						}))
+					}
+				/>
+				<Switch
+					checkedChildren="Show Matches"
+					unCheckedChildren="Hide Matches"
+					defaultChecked
+					onChange={(checked) =>
+						setIsShow((prevState) => ({
+							...prevState,
+							matches: checked,
+						}))
+					}
+				/>
+			</Space>
+
 			<section aria-label="ranking">
-				<Ranking rankingData={data.ranking} loading={loading} />
+				{isShow.ranking && (
+					<Ranking rankingData={data.ranking} loading={loading} />
+				)}
 			</section>
 
 			<section aria-label="match-history">
-				<MatchHistory matchesData={data.matches}/>
+				{isShow.matches && <MatchHistory matchesData={data.matches} />}
 			</section>
 		</>
 	);
