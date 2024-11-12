ffda110fbb3f19038f32d7fb03c06ed05beb9e2e
Thomas Dinh (Dinh Nguyen Son Tung) <work.dnstung@gmail.com>
2024-11-12 13:24:15 +0700

    config firebase credentials

---
+++ firebase/clientApp.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const clientCredentials = {
+  apiKey: "AIzaSyDoW_DoNpF78BOp_i98ie2qI2Y8zPONYYU",
+  authDomain: "pes-project-2a1a2.firebaseapp.com",
+  projectId: "pes-project-2a1a2",
+  storageBucket: "pes-project-2a1a2.firebasestorage.app",
+  messagingSenderId: "809931910737",
+  appId: "1:809931910737:web:80e922feca9fac05771270"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(clientCredentials);
