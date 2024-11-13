8022cc07e4330303bdc35c35feae99cdc6324d52
Thomas Dinh (Dinh Nguyen Son Tung) <work.dnstung@gmail.com>
2024-11-13 16:38:55 +0700

    create route to POST database

---
+++ src/app/api/match/route.js
@@ -0,0 +1,57 @@
+import connectMongoDB from "../../../../lib/mongodb";
+import Match from "../../../../models/match";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+	const {
+		match_number,
+		date,
+		time,
+		home_player,
+		home_team,
+		home_score,
+		away_player,
+		away_team,
+		away_score,
+	} = await req.json();
+	await connectMongoDB();
+
+	try {
+		await Match.create({
+			match_number,
+			date,
+			time,
+			home_player,
+			home_team,
+			home_score,
+			away_player,
+			away_team,
+			away_score,
+		});
+
+		return NextResponse.json({
+			success: true,
+			message: "Match Created",
+			match: {
+				match_number,
+				date,
+				time,
+				home_player,
+				home_team,
+				home_score,
+				away_player,
+				away_team,
+				away_score,
+			},
+		});
+	} catch (error) {
+		return NextResponse.json(
+			{
+				success: false,
+				message: "Failed to create match",
+				error: error.message,
+			},
+			{ status: 500 }
+		);
+	}
+}
