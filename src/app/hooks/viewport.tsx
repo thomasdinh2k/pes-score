e5e4443a0e372bbd824010fcf60a461710116400
Thomas Dinh (Dinh Nguyen Son Tung) <work.dnstung@gmail.com>
2024-11-18 17:30:32 +0700

    fix TS pre PR #3

--- src/app/hooks/viewport.tsx
+++ src/app/hooks/viewport.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 const useViewport = () => {
-	const [width, setWidth] = React.useState(window.innerWidth);
+	const [width, setWidth] = React.useState(
+		typeof window !== "undefined" ? window.innerWidth : 0
+	);
 
 	React.useEffect(() => {
-		const handleWindowResize = () => setWidth(window.innerWidth);
-		window.addEventListener("resize", handleWindowResize);
-		return () => window.removeEventListener("resize", handleWindowResize);
+		// Only run this code on the client side
+		if (typeof window !== "undefined") {
+			const handleWindowResize = () => setWidth(window.innerWidth);
+			window.addEventListener("resize", handleWindowResize);
+
+			// Clean up the event listener on component unmount
+			return () =>
+				window.removeEventListener("resize", handleWindowResize);
+		}
 	}, []);
 
 	return { width };
