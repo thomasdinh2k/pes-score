8e6097cf521eaf7c784e6fbafa70b159d45f496a
Thomas Dinh (Dinh Nguyen Son Tung) <work.dnstung@gmail.com>
2024-11-20 11:21:21 +0700

    intergrate google auth to store user authentication

--- src/app/api/match/[id]/route.js
+++ src/app/api/match/[id]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import connectMongoDB from "../../../../../lib/mongodb";
-import Match from "../../../../../models/match";
+import Match from "../../../models/match";
 
 export async function PUT(req, { params }) {
 	const { id } = await params;
